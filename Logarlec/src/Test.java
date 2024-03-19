fca79f8c0c14f4be0571ab63595379f40a5c9d03
AmerJusuf <Amerjusuf38@gmail.com>
2024-03-19 15:07:35 +0100

    empty methods initialized

--- Logarlec/src/Test.java
+++ Logarlec/src/Test.java
@@ -1,5 +1,71 @@
 public class Test {
-    public void StudentMoves(){
-        System.out.println("Student move test ... TODO");
-    }
+    public void studentMoves() {}
+
+    public void studentCannotMoveFullCapacity() {}
+
+    public void studentPicksItem() {}
+
+    public void instructorPicksItem() {}
+
+    public void studentCanNotPickItemFullInventory() {}
+
+    public void instructorCanNotPickItemFullInventory() {}
+
+    public void studentPicksLogarlex() {}
+
+    public void instructorCanNotPickLogarlec() {}
+
+    public void TVSZProtectsStudent() {}
+
+    public void TVSZProtectsStudentThenExpires() {}
+
+    public void camembertOpen() {}
+
+    public void instructorKicksStudent() {}
+
+    public void studentGetsCaughtButProtected() {}
+
+    public void studentGetsCaught() {}
+
+    public void instructorGetsCaught() {}
+
+    public void studentGetsDisabledButProtected() {}
+
+    public void studentGetsDisabled() {}
+
+    public void onlyInstructorGetsDisabled() {}
+
+    public void transistorSwitchOn() {}
+
+    public void transistorSwitchOff() {}
+
+    public void pairTransistorsBothNotPairedYet() {}
+
+    public void canNotPairTransistorsBecauseFirstIsPaired() {}
+
+    public void canNotPairTransistorsBecauseSecondIsPaired() {}
+
+    public void placeSecondTransistorAndMove() {}
+
+    public void placeFirstTransistorActiveAndPaired() {}
+
+    public void canNotPlaceTransistorBecauseNotActive() {}
+
+    public void canNotPlaceTransistorBecauseNotHaveAPair() {}
+
+    public void placeSecondTransistorActiveAndCanNotMoveBecauseOfCapacity() {}
+
+    public void poisonedRoomToxicatesCharacters() {}
+
+    public void mergeRooms() {}
+
+    public void splitRoom() {}
+
+    public void cursedRoomManagesDoors() {}
+
+    public void ragStunsInstructor() {}
+
+    public void beerProtectsStudent() {}
+
+    public void ragProtectsStudent() {}
 }
