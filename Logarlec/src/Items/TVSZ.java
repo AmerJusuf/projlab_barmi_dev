6e72c61897c2e9c7cd64e2e2b3494c0a75275899
Kapcsa Ákos Máté <akoskapcsa@gmail.com>
2024-03-21 15:07:46 +0100

    Nehany teszt irasa + Javadoc komment a Logarlecbe

--- Logarlec/src/Items/TVSZ.java
+++ Logarlec/src/Items/TVSZ.java
@@ -3,9 +3,9 @@ package Items;
 public class TVSZ extends Item {
     private int savesLeft;
 
-    public TVSZ() {
+    public TVSZ(int savesLeft) {
         super();
-        savesLeft = 3;
+        this.savesLeft = savesLeft;
     }
 
     public boolean protectStudent() {
@@ -19,8 +19,7 @@ public class TVSZ extends Item {
     }
 
     public void decreaseSavesLeft() {
-        System.out.println("TVSZ saves left: " + savesLeft + " | TVSZ: decreaseSavesLeft()");
         savesLeft--;
-        // if savesLeft is 0 expire the item
+        System.out.println("TVSZ saves left: " + savesLeft + " | TVSZ: decreaseSavesLeft()");
     }
 }
