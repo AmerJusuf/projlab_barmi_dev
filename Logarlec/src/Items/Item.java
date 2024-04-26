fad71b2b910d29b792fc0ae0fe6fa599824ad502
Botiy <horvath.botond425@gmail.com>
2024-04-26 16:46:53 +0200

    Statusok_esnehanyapromodositas

--- Logarlec/src/Items/Item.java
+++ Logarlec/src/Items/Item.java
@@ -130,4 +130,13 @@ public abstract class Item {
     public void step(){
         System.out.println("Item steps | Item: step()");
     }
+
+    public boolean isFake(){
+        if(isFake){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
